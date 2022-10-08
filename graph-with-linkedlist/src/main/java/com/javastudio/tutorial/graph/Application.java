503154f30e302dec343b9b4e0b597f60aa9c30ff
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2022-10-08 07:46:43 +0200

    3 project structure (#4)
    * Fix project structure
    
    * Graph with LinkedList
    
    Co-authored-by: Hossein Mohammadi <hmohammadi@mobiquityinc.com>

--- graph-with-linkedlist/src/main/java/com/javastudio/tutorial/graph/Application.java
+++ graph-with-linkedlist/src/main/java/com/javastudio/tutorial/graph/Application.java
@@ -1,11 +1,10 @@
-package com.javastudio.algorithm;
+package com.javastudio.tutorial.graph;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Application {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         LOGGER.info("Application started!");
     }
