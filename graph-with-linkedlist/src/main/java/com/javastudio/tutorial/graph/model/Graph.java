ec506d299106aaffd1e17fc23b218c37dda59357
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-10-08 09:26:54 +0200

    Graph with linedlist

--- graph-with-linkedlist/src/main/java/com/javastudio/tutorial/graph/model/Graph.java
+++ graph-with-linkedlist/src/main/java/com/javastudio/tutorial/graph/model/Graph.java
@@ -1,4 +1,41 @@
 package com.javastudio.tutorial.graph.model;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Graph {
+
+    private final HashMap<Node, LinkedList<Node>> adjacencyMap;
+    private final boolean directed;
+
+    public Graph(boolean directed) {
+        this.adjacencyMap = new HashMap<>();;
+        this.directed = directed;
+    }
+
+    public static class Node {
+        private final int value;
+        private boolean visited;
+
+        public Node(int value) {
+            this.value = value;
+            this.visited = false;
+        }
+
+        public void visit(){
+            this.visited = true;
+        }
+
+        public void unvisit(){
+            this.visited=false;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public boolean isVisited() {
+            return visited;
+        }
+    }
 }
